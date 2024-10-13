724f32a9dcea369ce9c4e6f7cc5367e21693cab3
Vikram Singh <vik.devmail@gmail.com>
2024-10-13 15:07:10 -0400

    Remove unused dependencies

--- Tuist/ProjectDescriptionHelpers/ExternalDependency.swift
+++ Tuist/ProjectDescriptionHelpers/ExternalDependency.swift
@@ -9,69 +9,21 @@ import Foundation
 import ProjectDescription
 
 public enum ExternalDependency: CaseIterable {
-    case activityIndicatorView
     case composableArchitecture
-    case concurrencyExtras
-    case dependencies
-    case firebaseAnalytics
-    case firebaseRemoteConfig
-    case floatingButton
-    case floatingPanel
-    case fuse
-    case grpc
-    case identifiedCollections
-    case keychainAccess
-    // case mediaPicker
     case rxrelay
     case rxswift
     case snapshotTesting
-    case supertokens
-    case swiftEntryKit
-    case swiftProtobuf
-    case swiftUIIntrospect
     
     public var name: String {
         return switch self {
         case .composableArchitecture:
             "ComposableArchitecture"
-        case .identifiedCollections:
-            "IdentifiedCollections"
         case .snapshotTesting:
             "SnapshotTesting"
-        case .firebaseAnalytics:
-            "FirebaseAnalytics"
-        case .firebaseRemoteConfig:
-            "FirebaseRemoteConfig"
-        case .fuse:
-            "Fuse"
-        case .concurrencyExtras:
-            "ConcurrencyExtras"
-        case .swiftEntryKit:
-            "SwiftEntryKit"
-        case .dependencies:
-            "Dependencies"
-        case .grpc:
-            "GRPC"
-        case .swiftProtobuf:
-            "SwiftProtobuf"
-        case .supertokens:
-            "SuperTokensIOS"
-        case .activityIndicatorView:
-            "ActivityIndicatorView"
-        // case .mediaPicker:
-        //     "ExyteMediaPicker"
-        case .floatingButton:
-            "FloatingButton"
-        case .swiftUIIntrospect:
-            "SwiftUIIntrospect"
-        case .keychainAccess:
-            "KeychainAccess"
         case .rxswift:
             "RxSwift"
         case .rxrelay:
             "RxRelay"
-        case .floatingPanel:
-            "FloatingPanel"
         }
     }
 
